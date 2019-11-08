b4c157159eb910dd1bd4436677922bbf5a229664
James Dewar <james@arcs.in>
2019-11-08 13:10:25 -0500

    3d grid

--- test/grid.test.js
+++ test/grid.test.js
@@ -8,15 +8,18 @@ const { Block } = require('../lib/block');
 test('Grids', t => {
     t.plan(8);
 
-    let grid, sizeX = 5, sizeY = 5, randomX = Math.round(Math.random() * (sizeX - 1)), randomY = Math.round(Math.random() * (sizeY - 1));
+    let grid, sizeX = 5, sizeY = 5, sizeZ = 5,
+        randomX = Math.round(Math.random() * (sizeX - 1)), 
+        randomY = Math.round(Math.random() * (sizeY - 1)),
+        randomZ = Math.round(Math.random() * (sizeZ - 1));
 
-    t.doesNotThrow(() => grid = new Grid(sizeX, sizeY), Error, 'Grid created successfully');
+    t.doesNotThrow(() => grid = new Grid(sizeX, sizeY, sizeZ), Error, 'Grid created successfully');
     t.ok(grid, 'Grid exists');
-    t.looseEqual(grid.size, {x: sizeX, y: sizeY}, 'Grid has the right size');
-    t.throws(() => grid.checkSquare(sizeX, sizeY), Error, "Can't access square outside grid");
-    t.doesNotThrow(() => grid.checkSquare(randomX, randomY), Error, 'Can access random square inside grid');
-    t.false(grid.getSquare(randomX, randomY), 'Randomly chosen square is empty');
+    t.looseEqual(grid.size, {x: sizeX, y: sizeY, z: sizeZ}, 'Grid has the right size');
+    t.throws(() => grid.checkCube(sizeX, sizeY, sizeZ), Error, "Can't access cube outside grid");
+    t.doesNotThrow(() => grid.checkCube(randomX, randomY, randomZ), Error, 'Can access random cube inside grid');
+    t.false(grid.getCube(randomX, randomY, randomZ), 'Randomly chosen cube is empty');
 
-    t.doesNotThrow(() => grid.fill(new Block({type: 'dirt', height: 0})), 'Grid filled with dirt');
-    t.looseEqual(grid.getSquare(randomX, randomY).info.type, 'dirt', 'Randomly chosen square is filled with dirt');
+    t.doesNotThrow(() => grid.fillArea(new Block({type: 'dirt'}), 0, 0, 0, sizeX - 1, sizeY - 1, 0), 'Bottom plane is filled with dirt');
+    t.looseEqual(grid.getCube(randomX, randomY, 0).info.type, 'dirt', 'Randomly chosen cube in plane is filled with dirt');
 });
